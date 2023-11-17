ec56999676d511de4e6addfd51bdaa9a739d147c
Santiago Nicolas Lopez <28.snll.05@gmail.com>
2023-11-16 22:56:43 -0500

    cambios de organizacion

---
+++ lib/src/domain/entities/Product.dart
@@ -0,0 +1,17 @@
+class Product {
+Product({
+  required this.id, 
+  required this.title,
+  required this.price,
+  required this.category,
+  required this.description,
+  required this.image,
+  });
+
+  int id ;
+  String title;
+  int price;
+  String category;
+  String description;
+  String image;
+}
