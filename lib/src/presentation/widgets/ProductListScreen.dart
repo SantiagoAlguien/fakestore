460e30cbde0b8f4da76278f56512bef145a27e5b
Santiago Nicolas Lopez <28.snll.05@gmail.com>
2023-11-19 22:17:22 -0500

    cambios de estilos

--- lib/src/presentation/widgets/ProductListScreen.dart
+++ lib/src/presentation/widgets/ProductListScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_call_super
+
 import 'package:fakestore/src/data/datasources/ProductRemoteDataSource.dart';
 import 'package:fakestore/src/data/repositories/ProductRepositoryImpl.dart';
 import 'package:fakestore/src/domain/entities/product/Product.dart';
@@ -16,9 +18,10 @@ class _ProductListScreenState extends State<ProductListScreen> {
   late Future<List<Product>> products;
   late GetProductUserCase getProductUserCase;
 
+  // ignore: must_call_super
   @override
-void initState() {
-  super.initState();
+  void initState() {
+  
   // Crea una instancia de ProductRemoteDataSource
   var productRemoteDataSource = ProductRemoteDataSource();
 
@@ -39,25 +42,38 @@ void initState() {
       appBar: AppBar(
         title: Text('Product List'),
       ),
-      body: FutureBuilder<List<Product>>(
-        future: products,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No products found'));
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(snapshot.data![index].title),
-              );
-            },
-          );
-        },
+      body: Container(
+        
+        child: FutureBuilder<List<Product>>(
+          future: products,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('No products found'));
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: MaterialButton(
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        Text(snapshot.data![index].title),
+                        Image.network(snapshot.data![index].image),
+                        
+                      ],
+                    )
+                  ),
+                  subtitle: Text(snapshot.data![index].category),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
